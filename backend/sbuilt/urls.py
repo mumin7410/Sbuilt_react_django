ad46b98326a1b45fe2caf59f33eb4ca353613e33
minstone <mumin96303@gmail.com>
2022-05-24 18:55:45 +0700

    make it better

--- backend/sbuilt/urls.py
+++ backend/sbuilt/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
     path('api/', include('myapp.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
