9855e5c882840719d240bd927705df10e155f514
minstone <mumin96303@gmail.com>
2022-05-25 11:06:18 +0700

    make it better

--- backend/sbuilt/settings.py
+++ backend/sbuilt/settings.py
@@ -136,9 +136,10 @@ MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = ['./build/static']
 
-STATIC_ROOT = os.path.join('./src/static')
-MEDIA_ROOT = os.path.join('./src/media')
-
+# STATIC_ROOT = os.path.join('./src/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# MEDIA_ROOT = os.path.join('./src/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
