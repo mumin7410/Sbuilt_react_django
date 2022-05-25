4684dd2bc37b675c296b9196eac24e89e0dd2c2c
minstone <mumin96303@gmail.com>
2022-05-25 13:04:27 +0700

    make it better

--- backend/sbuilt/settings.py
+++ backend/sbuilt/settings.py
@@ -131,16 +131,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = ['./build/static']
-
-# STATIC_ROOT = os.path.join('./src/static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# MEDIA_ROOT = os.path.join('./src/media')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_ROOT = os.path.join('./src/static')
+
+# MEDIA_ROOT = os.path.join('./src/media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
